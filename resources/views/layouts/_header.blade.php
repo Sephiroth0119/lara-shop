7de6395a4c6dc3a7ec082d764a2fb3f7fe17c9ae
vagrant <vagrant@homestead>
2020-06-09 04:03:16 +0000

    收货地址

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -17,35 +17,27 @@
 
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav navbar-right">
-                <!-- Authentication Links -->
+                <!-- 登录注册链接开始 -->
                 @guest
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                </li>
-                @if (Route::has('register'))
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                </li>
-                @endif
+                <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
                 @else
                 <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ Auth::user()->name }} <span class="caret"></span>
-                    </a>
-
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                        document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </div>
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
+                    {{ Auth::user()->name }}
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a href="{{ route('user_addresses.index') }}" class="dropdown-item">收货地址</a>
+                    <a class="dropdown-item" id="logout" href="#"
+                    onclick="event.preventDefault();document.getElementById('logout-form').submit();">退出登录</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                    </form>
+                </div>
                 </li>
                 @endguest
+                <!-- 登录注册链接结束 -->
             </ul>
         </div>
     </div>
