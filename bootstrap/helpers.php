704f4c86e49989799f808b8cd8042579da927644
vagrant <vagrant@homestead>
2020-06-09 02:40:55 +0000

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,4 +1,12 @@
 <?php
+
+// use Illuminate\Routing\Route;
+
 function test_helper(){
     return 'OK';
+}
+
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
 }
