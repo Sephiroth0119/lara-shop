d5e52989958b1993958c0e4b1597b203871b3f86
vagrant <vagrant@homestead>
2020-06-09 03:14:06 +0000

    邮箱验证功能

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/','PagesController@root')->name('root');
 
-Auth::routes();
+Auth::routes(['verify'=>true]);
 
-// Route::get('/home', 'HomeController@index')->name('home');
