08cc35462e1a963dc95a2c4010541b82b23c640e
vagrant <vagrant@homestead>
2020-06-18 07:16:15 +0000

    用户界面 - 商品详情页

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::redirect('/', '/products')->name('root');
-Route::get('products', 'ProductsController@index')->name('products.index');
 
-Auth::routes(['verify'=>true]);
+Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+
+Auth::routes(['verify' => true]);
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
-Route::group(['middleware' => ['auth', 'verified']], function() {
+Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
     Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
     Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
